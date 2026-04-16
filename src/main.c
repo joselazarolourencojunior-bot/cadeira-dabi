58a9b1672c63c4d22588870927177f7609cf8755
Lazaro <lazaro@cadeiradabi.com>
2026-04-16 10:02:11 -0300

    fix: rele desce assento em PB11

--- src/main.c
+++ src/main.c
@@ -47,21 +47,31 @@ typedef struct
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define OUT_SOBE_ASSENTO_PORT GPIOA
-#define OUT_SOBE_ASSENTO_PIN  GPIO_PIN_0
-#define OUT_DESCE_ENCOSTO_PORT GPIOA
-#define OUT_DESCE_ENCOSTO_PIN  GPIO_PIN_1
-#define OUT_SOBE_ENCOSTO_PORT GPIOA
-#define OUT_SOBE_ENCOSTO_PIN  GPIO_PIN_2
-#define OUT_DESCE_ASSENTO_PORT GPIOA
-#define OUT_DESCE_ASSENTO_PIN  GPIO_PIN_3
-#define OUT_REFLETOR_PORT GPIOA
-#define OUT_REFLETOR_PIN  GPIO_PIN_4
+#define BTN_SOBE_ASSENTO_PORT GPIOA
+#define BTN_SOBE_ASSENTO_PIN  GPIO_PIN_0
+#define BTN_DESCE_ENCOSTO_PORT GPIOA
+#define BTN_DESCE_ENCOSTO_PIN  GPIO_PIN_1
+#define BTN_SOBE_ENCOSTO_PORT GPIOA
+#define BTN_SOBE_ENCOSTO_PIN  GPIO_PIN_2
+#define BTN_DESCE_ASSENTO_PORT GPIOA
+#define BTN_DESCE_ASSENTO_PIN  GPIO_PIN_3
+#define BTN_REFLETOR_PORT GPIOA
+#define BTN_REFLETOR_PIN  GPIO_PIN_4
+#define BTN_VZ_PORT GPIOA
+#define BTN_VZ_PIN  GPIO_PIN_5
+#define BTN_PT_PORT GPIOA
+#define BTN_PT_PIN  GPIO_PIN_6
 
-#define IN_VZ_PORT GPIOA
-#define IN_VZ_PIN  GPIO_PIN_5
-#define IN_PT_PORT GPIOA
-#define IN_PT_PIN  GPIO_PIN_6
+#define RELE_SOBE_ASSENTO_PORT GPIOB
+#define RELE_SOBE_ASSENTO_PIN  GPIO_PIN_0
+#define RELE_DESCE_ENCOSTO_PORT GPIOB
+#define RELE_DESCE_ENCOSTO_PIN  GPIO_PIN_1
+#define RELE_SOBE_ENCOSTO_PORT GPIOB
+#define RELE_SOBE_ENCOSTO_PIN  GPIO_PIN_2
+#define RELE_DESCE_ASSENTO_PORT GPIOB
+#define RELE_DESCE_ASSENTO_PIN  GPIO_PIN_11
+#define RELE_REFLETOR_PORT GPIOB
+#define RELE_REFLETOR_PIN  GPIO_PIN_4
 
 #define WATCHDOG_TOGGLE_MS 500U
 #define BTN_DEBOUNCE_MS 50U
@@ -82,8 +92,17 @@ static SystemState_t systemState = SYSTEM_IDLE;
 static uint8_t vzCompletoCiclo = 0;
 static uint32_t operationStartMs = 0;
 
-static DebouncedButton_t btnVZ = { IN_VZ_PORT, IN_VZ_PIN, 0, 0, 0 };
-static DebouncedButton_t btnPT = { IN_PT_PORT, IN_PT_PIN, 0, 0, 0 };
+static uint8_t refletorOn = 0;
+
+static DebouncedButton_t btnSobeAssento = { BTN_SOBE_ASSENTO_PORT, BTN_SOBE_ASSENTO_PIN, 0, 0, 0 };
+static DebouncedButton_t btnDesceAssento = { BTN_DESCE_ASSENTO_PORT, BTN_DESCE_ASSENTO_PIN, 0, 0, 0 };
+static DebouncedButton_t btnSobeEncosto = { BTN_SOBE_ENCOSTO_PORT, BTN_SOBE_ENCOSTO_PIN, 0, 0, 0 };
+static DebouncedButton_t btnDesceEncosto = { BTN_DESCE_ENCOSTO_PORT, BTN_DESCE_ENCOSTO_PIN, 0, 0, 0 };
+static DebouncedButton_t btnRefletor = { BTN_REFLETOR_PORT, BTN_REFLETOR_PIN, 0, 0, 0 };
+static DebouncedButton_t btnVZ = { BTN_VZ_PORT, BTN_VZ_PIN, 0, 0, 0 };
+static DebouncedButton_t btnPT = { BTN_PT_PORT, BTN_PT_PIN, 0, 0, 0 };
+
+static uint8_t btnRefletorPrevStable = 0;
 static uint8_t btnVZPrevStable = 0;
 static uint8_t btnPTPrevStable = 0;
 
@@ -98,6 +117,7 @@ static void StartVoltaZero(uint32_t now);
 static void StartPosicaoTrabalho(uint32_t now);
 static void DebounceButton_Update(DebouncedButton_t *btn, uint32_t now);
 static void Buttons_Update(uint32_t now);
+static void ManualControl_Update(void);
 
 /* USER CODE END PFP */
 
@@ -105,16 +125,16 @@ static void Buttons_Update(uint32_t now);
 /* USER CODE BEGIN 0 */
 static void StopAllMotors(void)
 {
-  HAL_GPIO_WritePin(OUT_SOBE_ASSENTO_PORT,
-                    OUT_SOBE_ASSENTO_PIN | OUT_DESCE_ASSENTO_PIN | OUT_SOBE_ENCOSTO_PIN | OUT_DESCE_ENCOSTO_PIN,
+  HAL_GPIO_WritePin(RELE_SOBE_ASSENTO_PORT,
+                    RELE_SOBE_ASSENTO_PIN | RELE_DESCE_ASSENTO_PIN | RELE_SOBE_ENCOSTO_PIN | RELE_DESCE_ENCOSTO_PIN,
                     GPIO_PIN_RESET);
 }
 
 static void StartVoltaZero(uint32_t now)
 {
   StopAllMotors();
-  HAL_GPIO_WritePin(OUT_DESCE_ASSENTO_PORT, OUT_DESCE_ASSENTO_PIN, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(OUT_SOBE_ENCOSTO_PORT, OUT_SOBE_ENCOSTO_PIN, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(RELE_DESCE_ASSENTO_PORT, RELE_DESCE_ASSENTO_PIN, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(RELE_SOBE_ENCOSTO_PORT, RELE_SOBE_ENCOSTO_PIN, GPIO_PIN_SET);
   operationStartMs = now;
   systemState = SYSTEM_VOLTA_ZERO;
   vzCompletoCiclo = 0;
@@ -123,8 +143,8 @@ static void StartVoltaZero(uint32_t now)
 static void StartPosicaoTrabalho(uint32_t now)
 {
   StopAllMotors();
-  HAL_GPIO_WritePin(OUT_SOBE_ASSENTO_PORT, OUT_SOBE_ASSENTO_PIN, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(OUT_DESCE_ENCOSTO_PORT, OUT_DESCE_ENCOSTO_PIN, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(RELE_SOBE_ASSENTO_PORT, RELE_SOBE_ASSENTO_PIN, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(RELE_DESCE_ENCOSTO_PORT, RELE_DESCE_ENCOSTO_PIN, GPIO_PIN_SET);
   operationStartMs = now;
   systemState = SYSTEM_POSICAO_TRABALHO;
 }
@@ -151,8 +171,50 @@ static void DebounceButton_Update(DebouncedButton_t *btn, uint32_t now)
 
 static void Buttons_Update(uint32_t now)
 {
+  DebounceButton_Update(&btnSobeAssento, now);
+  DebounceButton_Update(&btnDesceAssento, now);
+  DebounceButton_Update(&btnSobeEncosto, now);
+  DebounceButton_Update(&btnDesceEncosto, now);
+  DebounceButton_Update(&btnRefletor, now);
   DebounceButton_Update(&btnVZ, now);
   DebounceButton_Update(&btnPT, now);
+}
+
+static void ManualControl_Update(void)
+{
+  if (btnSobeAssento.stable && !btnDesceAssento.stable)
+  {
+    HAL_GPIO_WritePin(RELE_SOBE_ASSENTO_PORT, RELE_SOBE_ASSENTO_PIN, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(RELE_DESCE_ASSENTO_PORT, RELE_DESCE_ASSENTO_PIN, GPIO_PIN_RESET);
+  }
+  else if (btnDesceAssento.stable && !btnSobeAssento.stable)
+  {
+    HAL_GPIO_WritePin(RELE_SOBE_ASSENTO_PORT, RELE_SOBE_ASSENTO_PIN, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(RELE_DESCE_ASSENTO_PORT, RELE_DESCE_ASSENTO_PIN, GPIO_PIN_SET);
+  }
+  else
+  {
+    HAL_GPIO_WritePin(RELE_SOBE_ASSENTO_PORT, RELE_SOBE_ASSENTO_PIN, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(RELE_DESCE_ASSENTO_PORT, RELE_DESCE_ASSENTO_PIN, GPIO_PIN_RESET);
+  }
+
+  if (btnSobeEncosto.stable && !btnDesceEncosto.stable)
+  {
+    HAL_GPIO_WritePin(RELE_SOBE_ENCOSTO_PORT, RELE_SOBE_ENCOSTO_PIN, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(RELE_DESCE_ENCOSTO_PORT, RELE_DESCE_ENCOSTO_PIN, GPIO_PIN_RESET);
+  }
+  else if (btnDesceEncosto.stable && !btnSobeEncosto.stable)
+  {
+    HAL_GPIO_WritePin(RELE_SOBE_ENCOSTO_PORT, RELE_SOBE_ENCOSTO_PIN, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(RELE_DESCE_ENCOSTO_PORT, RELE_DESCE_ENCOSTO_PIN, GPIO_PIN_SET);
+  }
+  else
+  {
+    HAL_GPIO_WritePin(RELE_SOBE_ENCOSTO_PORT, RELE_SOBE_ENCOSTO_PIN, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(RELE_DESCE_ENCOSTO_PORT, RELE_DESCE_ENCOSTO_PIN, GPIO_PIN_RESET);
+  }
+
+  HAL_GPIO_WritePin(RELE_REFLETOR_PORT, RELE_REFLETOR_PIN, refletorOn ? GPIO_PIN_SET : GPIO_PIN_RESET);
 }
 
 /* USER CODE END 0 */
@@ -205,16 +267,26 @@ int main(void)
     }
 
     Buttons_Update(now);
+    uint8_t refletorPressedEdge = (btnRefletor.stable && !btnRefletorPrevStable) ? 1 : 0;
     uint8_t vzPressedEdge = (btnVZ.stable && !btnVZPrevStable) ? 1 : 0;
     uint8_t ptPressedEdge = (btnPT.stable && !btnPTPrevStable) ? 1 : 0;
+    btnRefletorPrevStable = btnRefletor.stable;
     btnVZPrevStable = btnVZ.stable;
     btnPTPrevStable = btnPT.stable;
 
-    uint8_t anyButtonPressed = (btnVZ.stable || btnPT.stable) ? 1 : 0;
+    uint8_t anyButtonPressed = (btnSobeAssento.stable || btnDesceAssento.stable || btnSobeEncosto.stable ||
+                                btnDesceEncosto.stable || btnRefletor.stable || btnVZ.stable || btnPT.stable)
+                                   ? 1
+                                   : 0;
 
     switch (systemState)
     {
       case SYSTEM_IDLE:
+        if (refletorPressedEdge)
+        {
+          refletorOn = (uint8_t)!refletorOn;
+        }
+
         if (vzPressedEdge)
         {
           StartVoltaZero(now);
@@ -222,6 +294,10 @@ int main(void)
         else if (ptPressedEdge && vzCompletoCiclo)
         {
           StartPosicaoTrabalho(now);
+        }
+        else
+        {
+          ManualControl_Update();
         }
         break;
 
@@ -316,6 +392,7 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(Watch_Dog_GPIO_Port, Watch_Dog_Pin, GPIO_PIN_RESET);
@@ -328,20 +405,37 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(Watch_Dog_GPIO_Port, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
-  HAL_GPIO_WritePin(GPIOA,
-                    OUT_SOBE_ASSENTO_PIN | OUT_DESCE_ENCOSTO_PIN | OUT_SOBE_ENCOSTO_PIN | OUT_DESCE_ASSENTO_PIN | OUT_REFLETOR_PIN,
+  HAL_GPIO_WritePin(RELE_SOBE_ASSENTO_PORT,
+                    RELE_SOBE_ASSENTO_PIN | RELE_DESCE_ENCOSTO_PIN | RELE_SOBE_ENCOSTO_PIN | RELE_DESCE_ASSENTO_PIN | RELE_REFLETOR_PIN,
                     GPIO_PIN_RESET);
 
-  GPIO_InitStruct.Pin = OUT_SOBE_ASSENTO_PIN | OUT_DESCE_ENCOSTO_PIN | OUT_SOBE_ENCOSTO_PIN | OUT_DESCE_ASSENTO_PIN | OUT_REFLETOR_PIN;
+  GPIO_InitStruct.Pin = RELE_SOBE_ASSENTO_PIN | RELE_DESCE_ENCOSTO_PIN | RELE_SOBE_ENCOSTO_PIN | RELE_DESCE_ASSENTO_PIN | RELE_REFLETOR_PIN;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  HAL_GPIO_Init(RELE_SOBE_ASSENTO_PORT, &GPIO_InitStruct);
 
-  GPIO_InitStruct.Pin = IN_VZ_PIN | IN_PT_PIN;
+  GPIO_InitStruct.Pin = BTN_SOBE_ASSENTO_PIN | BTN_DESCE_ENCOSTO_PIN | BTN_SOBE_ENCOSTO_PIN | BTN_DESCE_ASSENTO_PIN | BTN_REFLETOR_PIN |
+                        BTN_VZ_PIN | BTN_PT_PIN;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  btnSobeAssento.lastRaw = (HAL_GPIO_ReadPin(btnSobeAssento.port, btnSobeAssento.pin) == GPIO_PIN_RESET) ? 1 : 0;
+  btnSobeAssento.stable = btnSobeAssento.lastRaw;
+
+  btnDesceAssento.lastRaw = (HAL_GPIO_ReadPin(btnDesceAssento.port, btnDesceAssento.pin) == GPIO_PIN_RESET) ? 1 : 0;
+  btnDesceAssento.stable = btnDesceAssento.lastRaw;
+
+  btnSobeEncosto.lastRaw = (HAL_GPIO_ReadPin(btnSobeEncosto.port, btnSobeEncosto.pin) == GPIO_PIN_RESET) ? 1 : 0;
+  btnSobeEncosto.stable = btnSobeEncosto.lastRaw;
+
+  btnDesceEncosto.lastRaw = (HAL_GPIO_ReadPin(btnDesceEncosto.port, btnDesceEncosto.pin) == GPIO_PIN_RESET) ? 1 : 0;
+  btnDesceEncosto.stable = btnDesceEncosto.lastRaw;
+
+  btnRefletor.lastRaw = (HAL_GPIO_ReadPin(btnRefletor.port, btnRefletor.pin) == GPIO_PIN_RESET) ? 1 : 0;
+  btnRefletor.stable = btnRefletor.lastRaw;
+  btnRefletorPrevStable = btnRefletor.stable;
 
   btnVZ.lastRaw = (HAL_GPIO_ReadPin(btnVZ.port, btnVZ.pin) == GPIO_PIN_RESET) ? 1 : 0;
   btnVZ.stable = btnVZ.lastRaw;
@@ -351,7 +445,6 @@ static void MX_GPIO_Init(void)
   btnPT.stable = btnPT.lastRaw;
   btnPTPrevStable = btnPT.stable;
 
-  /* USER CODE END MX_GPIO_Init_2 */
 
 /* USER CODE BEGIN 4 */
 
